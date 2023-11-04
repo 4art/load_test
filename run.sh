9238ac3fed894fe296d8233d5e76ff671cd3e1bc
metraf <firsov.tyoma@gmail.com>
2023-11-04 18:56:39 +0100

    fixed scale

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ docker build -t ddos .
 docker service create --name ddos-serv --detach=false ddos
 
 docker service ls
-docker service scale ddos-serv=250
+docker service scale ddos-serv=30
 
 docker service ls
